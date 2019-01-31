b07bf97d4d85fec2be06abdf0212070044356060
TakkuMattsu <3356758+TakkuMattsu@users.noreply.github.com>
2019-01-31 16:15:42 +0900

    CSVを読み込む

--- app/src/main/java/com/example/takkumattsu/rxmovie/MainActivity.kt
+++ app/src/main/java/com/example/takkumattsu/rxmovie/MainActivity.kt
@@ -2,11 +2,49 @@ package com.example.takkumattsu.rxmovie
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RawRes
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    data class MovieInfo (
+        val movieId: String,
+        val title: String,
+        val genres: String
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMovieCsv(R.raw.mini_movies)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Log.d("RxMovie", "$it")
+                }
+    }
+    private fun loadMovieCsv(@RawRes from: Int): Observable<MovieInfo> {
+        // csvを読み込む
+        // 1行づつ読み込んだやつをMovie dataクラスにして
+        // Observableで返す
+        return Observable.create<MovieInfo> { emitter ->
+            resources.openRawResource(R.raw.mini_movies)
+                    .bufferedReader()
+                    .use {
+                        var line: String? = it.readLine()
+                        while (line != null) {
+                            // ヘッダー行を飛ばす
+                            line = it.readLine()
+                            line?.let {
+                                val l = it.split(",")
+                                emitter.onNext(MovieInfo(l[0],l[1],l[2]))
+                            }
+                        }
+                        emitter.onComplete()
+                    }
+        }
     }
 }
