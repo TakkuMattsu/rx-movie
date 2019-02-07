ac141a164c4de8ddf127c6822c5d2b61781639b6
TakkuMattsu <3356758+TakkuMattsu@users.noreply.github.com>
2019-02-07 19:01:58 +0900

    kaptを利用

--- app/src/main/java/com/example/takkumattsu/rxmovie/MovieInfoDao.kt
+++ app/src/main/java/com/example/takkumattsu/rxmovie/MovieInfoDao.kt
@@ -1,9 +1,11 @@
 package com.example.takkumattsu.rxmovie
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 
+@Dao
 interface MovieInfoDao {
     @Query("SELECT * FROM MovieInfo")
     fun getAll(): List<MovieInfo>
